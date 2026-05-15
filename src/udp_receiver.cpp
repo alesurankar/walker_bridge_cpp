aa3b3d0bd4f92e05f172be2d12e1ad03832dac09
alesurankar <ale.urankar@gmail.com>
2026-05-15 17:28:00 +0200

    commit2

--- src/udp_receiver.cpp
+++ src/udp_receiver.cpp
@@ -1,16 +1,19 @@
 #include "walker_bridge_cpp/udp_receiver.hpp"
 #include <nlohmann/json.hpp>
 
+
 using boost::asio::ip::udp;
 
-static constexpr int PORT = 17945;
-
 UdpReceiver::UdpReceiver()
-: Node("udp_receiver"),
-  socket_(io_context_, udp::endpoint(udp::v4(), PORT))
+  : 
+  Node("udp_receiver"),
+  socket_(io_context_, udp::endpoint(udp::v4(), PORT)),
+  work_guard_(boost::asio::make_work_guard(io_context_))
 {
   pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
     "/humanoid/joint_targets", 10);
+
+  RCLCPP_INFO(this->get_logger(), "UDP Receiver started on port %d", PORT);
 
   start_receive();
 
@@ -36,23 +39,29 @@ void UdpReceiver::start_receive()
     remote_endpoint_,
     [this, buffer](const boost::system::error_code &ec, std::size_t bytes)
     {
-      if (!ec)
+      if (!ec && bytes > 0)
       {
         try
         {
           std::string data(buffer->data(), bytes);
+
           auto j = nlohmann::json::parse(data);
 
           std_msgs::msg::Float32MultiArray msg;
+          msg.data.clear();
 
           for (auto &v : j["joint_positions"])
+          {
             msg.data.push_back(v.get<float>());
+          }
 
           pub_->publish(msg);
         }
-        catch (...) {}
+        catch (const std::exception &e)
+        {
+          RCLCPP_ERROR(this->get_logger(), "JSON parse error: %s", e.what());
+        }
       }
-
       start_receive();
     });
 }
