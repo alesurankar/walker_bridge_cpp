07a720b67a63fb5582c55647630b2c2f517c9edf
alesurankar <ale.urankar@gmail.com>
2026-05-19 05:42:39 +0200

    now use custom_ros_messages

--- include/udp_ros_bridge/udp_bridge_node.hpp
+++ include/udp_ros_bridge/udp_bridge_node.hpp
@@ -3,11 +3,13 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_msgs/msg/empty.hpp>
 
+#include <motion_interfaces/msg/base_velocity_command.hpp>
+#include <motion_interfaces/msg/joint_position_command.hpp>
 #include <motion_interfaces/msg/cartesian_pose_command.hpp>
+#include <motion_interfaces/msg/stop_command.hpp>
 #include "udp_ros_bridge/udp_receiver.hpp"
 #include "udp_ros_bridge/command_router.hpp"
 #include "udp_ros_bridge/decoders/json_command_decoder.hpp"
-#include "udp_ros_bridge/protocol/command_message.hpp"
 
 #include <memory>
 #include <thread>
@@ -21,19 +23,19 @@ public:
   ~UdpBridgeNode();
 private:
   void consumer_loop();
-  void publish_base_velocity(const udp_ros_bridge::BaseVelocity& bv);
-  void publish_joint_state(const udp_ros_bridge::JointPosition& jp);
-  void publish_cartesian_pose(const udp_ros_bridge::CartesianPoseCommand& cp);
-  void publish_stop();
+  void publish_base_velocity(const motion_interfaces::msg::BaseVelocityCommand& msg);
+  void publish_joint_state(const motion_interfaces::msg::JointPositionCommand& msg);
+  void publish_cartesian_pose(const motion_interfaces::msg::CartesianPoseCommand& msg);
+  void publish_stop(const motion_interfaces::msg::StopCommand& msg);
 private:
   UdpReceiver udp_;
   udp_ros_bridge::JsonCommandDecoder decoder_;
   udp_ros_bridge::CommandRouter router_;
 
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr base_pub_;
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_pub_;
+  rclcpp::Publisher<motion_interfaces::msg::BaseVelocityCommand>::SharedPtr base_pub_;
+  rclcpp::Publisher<motion_interfaces::msg::JointPositionCommand>::SharedPtr joint_pub_;
   rclcpp::Publisher<motion_interfaces::msg::CartesianPoseCommand>::SharedPtr pose_pub_;
-  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr stop_pub_;
+  rclcpp::Publisher<motion_interfaces::msg::StopCommand>::SharedPtr stop_pub_;
   std::thread consumer_thread_;
   std::atomic<bool> running_{false};
 };
