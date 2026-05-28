85efc143e935573b9d841a7d4a98654d2854720e
alesurankar <ale.urankar@gmail.com>
2026-05-28 08:51:27 +0200

    add beter udp_sender test

--- test_tools/udp_sender.py
+++ test_tools/udp_sender.py
@@ -1,88 +1,41 @@
 import socket
 import json
 import time
-import math
-
+import random
+import os
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 17945
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+print("DATA_DIR =", DATA_DIR)
+
+def load_poses():
+    poses = []
+    for file in os.listdir(DATA_DIR):
+        if file.endswith(".json"):
+            path = os.path.join(DATA_DIR, file)
+            with open(path, "r") as f:
+                poses.append(json.load(f))
+    return poses
+
+poses = load_poses()
+
+if len(poses) == 0:
+    raise RuntimeError("No pose files found in data/")
+
+print(f"Loaded {len(poses)} poses")
+
 start = time.monotonic()
 
-# Initial robot state
-joint_msg = {
-  "header": {
-    "robot_id": 1,
-    "type": "joint_position",
-    "priority": 1,
-  },
-  "payload": {
-    "names": [
-      "waist_yaw_joint",
-      "waist_pitch_joint",
-      "head_yaw_joint",
-      "head_pitch_joint",
-      "L_shoulder_pitch_joint",
-      "L_shoulder_roll_joint",
-      "L_shoulder_yaw_joint",
-      "L_elbow_roll_joint",
-      "L_elbow_yaw_joint",
-      "L_wrist_pitch_joint",
-      "L_wrist_roll_joint",
-      "R_shoulder_pitch_joint",
-      "R_shoulder_roll_joint",
-      "R_shoulder_yaw_joint",
-      "R_elbow_roll_joint",
-      "R_elbow_yaw_joint",
-      "R_wrist_pitch_joint",
-      "R_wrist_roll_joint",
-      "L_hip_roll_joint",
-      "L_hip_yaw_joint",
-      "L_hip_pitch_joint",
-      "L_knee_pitch_joint",
-      "L_ankle_pitch_joint",
-      "L_ankle_roll_joint",
-      "R_hip_roll_joint",
-      "R_hip_yaw_joint",
-      "R_hip_pitch_joint",
-      "R_knee_pitch_joint",
-      "R_ankle_pitch_joint",
-      "R_ankle_roll_joint"
-    ],
-    "positions": [
-      0.0, 0.1, 0.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-      0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-    ]
-  }
-}
-
-# STEP 1: establish current robot state
-print("Sending initial joint state...")
-
-sock.sendto(
-  json.dumps(joint_msg).encode("utf-8"),
-  (UDP_IP, UDP_PORT)
-)
-# Give MoveIt time to receive /joint_states
-time.sleep(1.5)
-
-
-# STEP 2: send cartesian target
-BASE_X = 0.30
-BASE_Y = 0.00
-BASE_Z = 0.25
-
-SINE_AMPLITUDE = 0.01
-SEND_PERIOD = 2.0  
-
 while True:
+  t = time.monotonic() - start
 
-  t = time.monotonic()
-  elapsed = t - start
+  # pick random pose
+  payload = random.choice(poses)
 
   pose_msg = {
     "header": {
@@ -90,27 +43,14 @@ while True:
       "type": "cartesian_pose",
       "priority": 1,
     },
-    "payload": {
-      "target_link": "L_sixforce_link",
-      "frame_id": "base_link",
-      "x": 0.25,
-      "y": 0.0,
-      "z": 0.25,
-      "qx": 0.0,
-      "qy": 0.0,
-      "qz": 0.0,
-      "qw": 1.0,
-      "position_gain": 0.5,
-      "orientation_gain": 0.5,
-      "is_relative": False
-    }
+    "payload": payload
   }
-
-  print("Sending cartesian pose...")
 
   sock.sendto(
     json.dumps(pose_msg).encode("utf-8"),
     (UDP_IP, UDP_PORT)
   )
+
+  print(f"Sent pose: x={payload['x']:.3f}, y={payload['y']:.3f}, z={payload['z']:.3f}")
 
   time.sleep(10)
